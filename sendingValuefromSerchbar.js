e90dbfab5a141e84bd8b3e9050abdbc7e774ecbc
shashwat shukla <shuklashashwat009@gmail.com>
2024-01-31 17:54:36 +0530

    commit two

---
+++ sendingValuefromSerchbar.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const app=express()
+const port =3000;
+app.get('/', (req,res)=>{//will always work doesnt matter what you put after the / in the url bar 
+    res.send("hello")
+})
+
+app.post('/sum', (req,res)=>{
+    var count=req.query.counter;
+    let tot=10;
+    for (let i = 0;i <count; i++) {
+        tot=tot+i;
+    }
+
+    res.send(tot.toString())
+})
+app.listen(port,()=>{
+    console.log("hello everyone");
+})
