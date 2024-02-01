a659d9e6065f2ab01bfa9fb8b0acf594b74ab12b
shashwat shukla <shuklashashwat009@gmail.com>
2024-02-02 02:16:00 +0530

    2 feb

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const express=require("express")
+var bodyParser=require('body-parser')
 const app=express()
 const port =3000;
+app.use(bodyParser.json())
 app.get('/', (req,res)=>{//will always work doesnt matter what you put after the / in the url bar 
     res.send("hello")
 })
 
 app.post('/sum', (req,res)=>{
-    var count=req.headers.counter;
+    var count=req.body.age;
     let tot=10;
     for (let i = 0;i <count; i++) {
         tot=tot+i;
     }
 
-    res.send(tot.toString())
+    console.log(req.body);
+
+    res.send(tot.toString()) 
 })
 app.listen(port,()=>{
     console.log("hello everyone");
-})
+})  
