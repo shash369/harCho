090192aacf43bdad7893ad9c7c1fb3e8a8b4a5dd
shashwat shukla <shuklashashwat009@gmail.com>
2024-02-15 02:34:57 +0530

    post method of todo app is completed

---
+++ todo2/main.js
@@ -0,0 +1,26 @@
+const express =require('express')
+const bodyParser=require('body-parser')
+const app=express()
+app.use(bodyParser.json())
+const port=3003;
+
+
+app.get('/todos',(req,res)=>{
+   res.send("Todos will be added here");
+})
+const todos=[];
+let count=1;
+app.post('/todos',(req,res)=>{
+    const newTodo={
+        id:  count,
+        title:req.body.title,
+        description:req.body.description
+    }
+     count++
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+})
+
+app.listen(port,()=>{
+    console.log("port is lisning at http://localhost:3003/todos")
+})
