3e51e78e88cd7b7aa8b5695b0f765f717c3a32b2
shashwat shukla <shuklashashwat009@gmail.com>
2024-02-17 00:01:55 +0530

    delete path is added for that two function is created and added

--- todo2/main.js
+++ todo2/main.js
@@ -4,12 +4,49 @@ const app=express()
 app.use(bodyParser.json())
 const port=3003;
 
+let todos=[];
+
+function findIndex(arr,id){
+    for (let i = 0; i < arr.length; i++) {
+       if(arr[i].id===id){
+        return i
+       }
+        else{
+            return -1
+        }
+    }
+}
+// function findIndex(arr, id) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].id === id) {
+//             return i;
+//         }
+//     }
+//     return -1;  // Move this line outside the loop
+// }
+
+// function removeAtIndex(arr ,id){
+//     let newArray=[];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].id!=id) {
+//             newArray.push(arr[i])
+//         }
+        
+//     }
+//     return newArray
+// }
+function removeAtIndex(arr, index) {
+    arr.splice(index, 1);
+    return arr;
+}
+
+
+
+let count=1;
 
 app.get('/todos',(req,res)=>{
-   res.send("Todos will be added here");
-})
-const todos=[];
-let count=1;
+    res.send(todos);
+ })
 app.post('/todos',(req,res)=>{
     const newTodo={
         id:  count,
@@ -21,7 +58,17 @@ app.post('/todos',(req,res)=>{
     res.status(201).json(newTodo);
 })
 
-//dfvc
+app.delete('/todos/:id',(req,res)=>{
+    const ID=findIndex(todos,parseInt(req.params.id))//return of req.params.id is a string
+    if (ID === -1) {
+        res.status(404).send("entered id not found")
+    }
+    else{
+      todos=removeAtIndex(todos,ID);
+    }
+    res.status(200).send("deletion is succesfull");
+})
+
 app.listen(port,()=>{
     console.log("port is lisning at http://localhost:3003/todos")
 })
