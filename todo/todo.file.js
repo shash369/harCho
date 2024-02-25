7e927ba50d2b4550568912bc3ceee0d98ec69c24
shashwat shukla <shuklashashwat009@gmail.com>
2024-02-25 23:17:21 +0530

    garbage

--- todo/todo.file.js
+++ todo/todo.file.js
@@ -16,6 +16,23 @@ app.get('/',(req,res)=>{
    })
 })
 
+app.post('/todos',(req,res)=>{
+  let newTodo={
+    id:Math.floor(Math.random() * 1000000),
+    title:req.params.title,
+    description:req.params.description
+  }
+  fs.readFile('todos.txt',"utf-8",(err,data)=>{
+    if(err) throw err
+    let todo=JSON.parse(data)
+    todo.push(newTodo)
+    fs.writeFile('todos.txt',JSON.stringify(todo),(err)=>{
+      if(err) throw err
+
+      res.send("OK")
+    })
+  })
+})
 
 app.listen(port,()=>{
     console.log("server is at"+`${port}`);
